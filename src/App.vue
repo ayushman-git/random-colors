e8e30d341146a16ec6cc0d338a9828bebcccbabf
Ayushman Gupta <ayushman1998.1998@gmail.com>
2020-08-21 16:54:35 +0530

    added randomize button

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <random-button v-on:clicked="randomizeColor" :randomColor="randomColor"></random-button>
+  </div>
+</template>
+
+<script>
+import Button from "./components/Button.vue";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      randomColor: null,
+    };
+  },
+  components: {
+    "random-button": Button,
+  },
+  methods: {
+    randomizeColor() {
+      this.randomColor = "#" + Math.random().toString().slice(2, 8);
+      console.log(this.randomColor);
+    },
+  },
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Maven+Pro:wght@900&display=swap');
+* {
+  box-sizing: border-box;
+}
+
+body {
+  padding: 0;
+  margin: 0;
+}
+
+#app {
+  display: flex;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+}
+</style>
---
+++ src/components/Button.vue
@@ -0,0 +1,44 @@
+<template>
+  <button @click="$emit('clicked')" :style="{'background-color': randomColor}">Randomize
+    <p>{{ randomColor }}</p>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+
+  props: {
+    randomColor: {
+      type: String,
+    },
+  },
+};
+</script>
+
+<style scoped>
+button {
+  font-family: 'Maven Pro', sans-serif;
+  color: white;
+  font-size: 3em;
+  width: 7em;
+  height: 7em;
+  border-radius: 50%;
+  border: none;
+  box-shadow: 0px 0px 41px -1px rgba(0,0,0,0.49);
+  transition: box-shadow 0.2s linear;
+  mix-blend-mode: difference;
+}
+
+button:hover {
+  box-shadow: 0px 0px 57px 2px rgba(0,0,0,0.72);
+}
+
+button:focus {
+  outline: none;
+}
+
+p {
+  font-size: 14px;
+}
+</style>
